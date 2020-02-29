be7ad02aec2789d9f3fcb5033a6530b095ab2d95
ashakirin <andrei.shakirin@gmail.com>
2020-02-29 18:34:55 +0100

    Updated circuit breaker

---
+++ circuitbreaker/composite-service/src/main/java/com/talend/microservices/patterns/democompositeservice/BasicServiceClient.java
@@ -0,0 +1,42 @@
+package com.talend.microservices.patterns.democompositeservice;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class BasicServiceClient {
+    private static final String HYSTRIX_DEFAULT_CONFIG = "default";
+    Logger LOGGER = LoggerFactory.getLogger(BasicServiceClient.class);
+    private static final String HYSTRIX_HELLO = "hello";
+    private static final String DEFAULT_HYSTRIX_CONFIG = "default";
+    private final String basicHelloUrl;
+
+    public BasicServiceClient(@Value("${basic.hello.url}") final String basicHelloUrl) {
+        this.basicHelloUrl = basicHelloUrl;
+    }
+
+    @HystrixCommand(fallbackMethod = "defaultFallback",
+            groupKey = HYSTRIX_DEFAULT_CONFIG,
+            commandKey = HYSTRIX_DEFAULT_CONFIG
+    )
+    public String invokeBasic() {
+        try {
+            LOGGER.info("Invoking basic service ...");
+            WebClient webClient = WebClient.builder().baseUrl(basicHelloUrl).build();
+            Mono<String> response = webClient.get().uri("basic/hello").retrieve().bodyToMono(String.class);
+            return response.block();
+        } catch (RuntimeException e) {
+            LOGGER.error("Error due basic service call: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    private String defaultFallback(final Throwable e) {
+        return "Hello from composite fallback: " + e.toString();
+    }
+}
